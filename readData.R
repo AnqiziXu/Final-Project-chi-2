429aeca0b40177f9e58dc8cbc67c8942df4d5ceb
Anqizi Xu <anqizx@uw.edu>
2019-06-03 15:11:08 -0700

    filtering useful data Close #7

--- readData.R
+++ readData.R
@@ -1,3 +1,12 @@
 library(dplyr)
 
 data <- read.csv("./data/kc_house_data.csv", stringsAsFactors = FALSE)
+
+# filter out the houses that are extremely big or small, and 
+# also clean out some of the unnecessary columns 
+suited_house <- data %>%
+  filter (bedrooms >1 & bedrooms <6
+          & bathrooms > 1 & bathrooms <6) %>%
+  select(-floors, -sqft_lot, -waterfront, 
+         -grade, -sqft_living15,
+         -sqft_lot15, -view)
