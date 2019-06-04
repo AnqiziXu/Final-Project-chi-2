d9fd68131dc0f0e13a791b8de726c3e0cf498902
Leonardo <leorojas@uw.edu>
2019-06-03 20:19:25 -0700

    This adds correction to dates

--- Data/readData.R
+++ Data/readData.R
@@ -1,6 +1,6 @@
 library(dplyr)
 
-data <- read.csv("./data/kc_house_data.csv", stringsAsFactors = FALSE)
+data <- read.csv("../Data/kc_house_data.csv", stringsAsFactors = FALSE)
 
 # filter out the houses that are extremely big or small, and 
 # also clean out some of the unnecessary columns 
@@ -10,6 +10,7 @@ suited_house <- data %>%
   select(-floors, -sqft_lot, -waterfront, 
          -grade, -sqft_living15,
          -sqft_lot15, -view)
+suited_house$date  <- (substr(suited_house$date, 1, 8))
 
 write.csv(suited_house, file = "suited_house.csv")
 
