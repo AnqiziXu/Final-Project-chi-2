e6ea96cdb82a7666b28b2dd77b21344004cfa434
Kevinfangsw <fangs6@uw.edu>
2019-06-04 21:20:43 -0800

    filters created

---
+++ Shiny/filter.R
@@ -0,0 +1,18 @@
+source('../data/readData.R')
+
+conditionFilter <- function(inputData) {
+  # function that takes list of input value as parameters, and return the filtered data
+
+  
+  priceRange <- inputData$price
+  sqftLiv <- inputData$sqft
+
+  filtered <- suited_house %>% 
+    filter(bedrooms <= inputData$rooms & bathrooms <= inputData$bath 
+           & zipcode == inputData$Zip & between(price, priceRange[1], priceRange[2])
+            & condition <= inputData$condition & between(sqft_living, sqftLiv[1], sqftLiv[2]))
+
+  
+  return(filtered)
+}
+
