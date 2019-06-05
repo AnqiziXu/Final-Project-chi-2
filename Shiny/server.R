d0264cbc3ee344b4724e8a0c997211163a6997aa
Leonardo <leorojas@uw.edu>
2019-06-05 11:03:22 -0700

    Fixed the popup so it shows the relevant information for each of the selected houses

--- Shiny/server.R
+++ Shiny/server.R
@@ -10,14 +10,23 @@ housing <- read.csv("../Data/suited_house.csv", stringsAsFactors = FALSE)
 source('../Data/readData.R')
 
 shinyServer(function(input, output) {
-   
+
+  data <- reactive({
+    x <- df
+  })
+     
   output$mapPlot <- renderLeaflet({
-    leaflet(conditionFilter(input)) %>%
+    df <- conditionFilter(input)
+    
+    leaflet(df) %>%
       addTiles(options = providerTileOptions(noWrap = TRUE)) %>%
       setView(lng=-122.3 , lat = 47.6, zoom= 12) %>% 
       addMarkers(lat = ~lat,
                  lng = ~long,
-                 popup = paste(conditionFilter(input)))
+                 popup = paste("Price:",df$price,"<br>","Number of Rooms:",df$bedrooms,"<br>",
+                               "Number of Bathrooms:",df$bathrooms,"<br>",
+                               "Square Footage of Living Space:",df$sqft_living,"<br>",
+                               "Month Sold:",df$month))
   })
   
   output$test <- renderText({
