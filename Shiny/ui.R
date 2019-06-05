854e73fe82444dfe47e5b76e0b393a3ff4b39343
Leonardo <leorojas@uw.edu>
2019-06-05 12:08:37 -0700

    I have added slider selectors for the Summary page

--- Shiny/ui.R
+++ Shiny/ui.R
@@ -50,7 +50,6 @@ shinyUI(navbarPage("House Sales in 2014 and 2015", theme = shinytheme("flatly"),
                    "Living Square Footage",
                    min = 0,
                    max = 10000,
-                   # why are you changing this to one side slider?
                    value = c(0, 4000))
     ),
     
@@ -61,8 +60,44 @@ shinyUI(navbarPage("House Sales in 2014 and 2015", theme = shinytheme("flatly"),
       )
     )
   ),
-  tabPanel("Summary",
-           mainPanel("Summary dats",dataTableOutput("dataTable")))
+  
+   tabPanel("Summary",
+            sidebarLayout(
+              sidebarPanel(
+                sliderInput("rooms",
+                            "Number of bedrooms:",
+                            min = 2,
+                            max = 6,
+                            value = 4),
+                sliderInput("bath",
+                            "Number of bathrooms:",
+                            min = 2,
+                            max = 6,
+                            value = 4),
+                numericInput("condition",
+                             label = "Zip Code",
+                             value = 98122,
+                             min = 98001,
+                             max = 98199),
+                sliderInput("price",
+                            "Price Range",
+                            min = 0,
+                            max = 1000000,
+                            value = c(50000, 600000)),
+                sliderInput("condition",
+                            "Condition of House",
+                            min = 0,
+                            max = 5,
+                            value = 4),
+                sliderInput("sqft",
+                            "Living Square Footage",
+                            min = 0,
+                            max = 10000,
+                            value = c(0, 4000))
+              ),
+           mainPanel("Summary data",dataTableOutput("dataTable")))
+           
+   )
 ))
 
 
